a22fff90210f322b3b9ecfaefea99286d736c302
William Austin <william@voiceit.io>
2023-10-18 15:49:59 -0400

    QR-482: GitHub CVE Scanning
    Add nlohmann json build script.

---
+++ build.ps1
@@ -0,0 +1,15 @@
+#! /usr/bin/pwsh
+
+Remove-Item -Path .\build -Recurse -Force
+
+New-Item -Path .\build -ItemType Directory
+
+Set-Location -Path .\build\
+
+cmake ..
+
+& "C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/msbuild.exe" nlohmann_json.sln -target:Clean
+
+& "C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/msbuild.exe" nlohmann_json.sln
+
+Set-Location ..\
